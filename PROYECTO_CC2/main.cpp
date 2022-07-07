39b233536cd67106977b65199907672e1ea7ac75
Roberto Gustavo Yavar Guillen <104165295+GustavoYG@users.noreply.github.com>
2022-07-06 21:39:11 -0500

    Update main.cpp

--- PROYECTO_CC2/main.cpp
+++ PROYECTO_CC2/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include "iostream"
 #include "Player.h"
 #include "Platform.h"
@@ -8,7 +8,7 @@
 using namespace sf;
 using namespace std;
 
-static const float VIEW_HEIGHT = 512.0f;
+static const float VIEW_HEIGHT = 400.0f;//tamaño de la vista
 
 int main()
 {
@@ -17,21 +17,47 @@ int main()
     // primer vector es el centro, y el resto su tamaño de la vista
     View * view = new View(Vector2f(0.0f, 0.0f), Vector2f(VIEW_HEIGHT, VIEW_HEIGHT));
 
-    // creamos un un jugador
-    Player* dinitrio = new Player("DinitrioSprite.png", Vector2u(3, 2), "arrows", 0.3f, 200.0f, 100.0f);
-    Player* dinoncio = new Player("DinitrioSprite.png", Vector2u(3, 2), "letters", 0.3f, 200.0f, 100.0f);
+    // Creando los jugadores
+    Player* dinitrio = new Player("DinitrioSprite.png", Vector2u(3, 2), "arrows", 0.3f, 150.0f, 50.0f);
+    Player* dinoncio = new Player("DinitrioSprite.png", Vector2u(3, 2), "letters", 0.3f, 150.0f, 50.0f);
 
     vector<Player> players;
     players.push_back(*dinitrio);
     players.push_back(*dinoncio);
 
-
+    //NIVEL1
     // vector de plataformas
     vector<Platform> platforms;
-    platforms.push_back(Platform("rock.jpg", Vector2f(100.0f, 200.0f), Vector2f(500.0f, 200.0f)));
-    platforms.push_back(Platform("rock.jpg", Vector2f(400.0f, 200.0f), Vector2f(500.0f, 0.0f)));
-    platforms.push_back(Platform("rock.jpg", Vector2f(1000.0f, 200.0f), Vector2f(0.0f, 600.0f)));
+    //rock left
+    platforms.push_back(Platform("rock.jpg", Vector2f(20.0f, 1200.0f), Vector2f(0.0f, 600.0f)));
+    //rock right
+    platforms.push_back(Platform("rock.jpg", Vector2f(20.0f, 1200.0f), Vector2f(600.0f, 600.0f)));
+    //roca suelo
+    platforms.push_back(Platform("rock.jpg", Vector2f(400.0f, 200.0f), Vector2f(200.0f, 600.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(100.0f, 200.0f), Vector2f(550.0f, 600.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(50.0f, 50.0f), Vector2f(570.0f, 480.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(30.0f, 70.0f), Vector2f(580.0f, 450.0f)));
+    //superficie01
+    platforms.push_back(Platform("rock.jpg", Vector2f(50.0f, 20.0f), Vector2f(35.0f, 380.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(10.0f, 20.0f), Vector2f(150.0f, 380.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(50.0f, 20.0f), Vector2f(300.0f, 380.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(50.0f, 20.0f), Vector2f(470.0f, 380.0f)));
+    //superficie02
+    platforms.push_back(Platform("rock.jpg", Vector2f(400.0f, 20.0f), Vector2f(400.0f, 250.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(30.0f, 5.0f), Vector2f(150.0f, 260.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(30.0f, 5.0f), Vector2f(100.0f, 390.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(30.0f, 5.0f), Vector2f(100.0f, 310.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(30.0f, 30.0f), Vector2f(20.0f, 355.0f)));
+    //superficiefinal
+    platforms.push_back(Platform("rock.jpg", Vector2f(30.0f, 5.0f), Vector2f(575.0f, 210.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(15.0f, 5.0f), Vector2f(500.0f, 165.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(15.0f, 5.0f), Vector2f(400.0f, 145.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(15.0f, 5.0f), Vector2f(300.0f, 125.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(15.0f, 5.0f), Vector2f(200.0f, 105.0f)));
+    platforms.push_back(Platform("rock.jpg", Vector2f(60.0f, 5.0f), Vector2f(20.0f, 120.0f)));
+    platforms.push_back(Platform("puerta.png", Vector2f(30.0f, 60.0f), Vector2f(5.0f, 89.0f)));
 
+    
 
     GameScene* scene = new GameScene(window,view,VIEW_HEIGHT,players,platforms);
 
@@ -40,7 +66,6 @@ int main()
     {
         scene->loop();
     }
-
 
     return 0;
 }
